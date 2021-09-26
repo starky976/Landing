59e292ee31b445a060278cb5a4c6b2b6980911ed
starky976 <56622658+starky976@users.noreply.github.com>
2021-09-26 22:47:56 +0900

    initial commit
    initial commit

---
+++ js/humburger.js
@@ -0,0 +1,25 @@
+$(function () {
+  let timer = 0 ;
+  $(".js-humburger--bt").click( function () {
+    $(this).toggleClass("is-open");
+    $(".js-humburger").toggleClass("is-open");
+    $("body").toggleClass("is-open");
+    //console.log("createClass");
+  });
+  $(window).on("resize",function () {
+    let pcWidth = 769;
+    if ( timer > 0 ) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      let resizeWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      if (resizeWidth >= pcWidth) {
+        //
+        //console.log("deleteClass");
+        $(".js-humburger").removeClass("is-open");
+        $(".js-humburger--bt").removeClass("is-open");
+        $("body").removeClass("is-open");
+      }
+    }, 200);
+  });
+});
